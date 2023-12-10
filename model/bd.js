541da5aa2535e07ebcffc0fce57b9b901f6dbf20
FelipeCoutinhoBras <felipebrascoutinho@gmail.com>
2023-12-10 09:43:52 -0300

    Criando as tabelas do banco de dados.

--- model/bd.js
+++ model/bd.js
@@ -1,8 +1,69 @@
-const Sequelize = require('sequelize')
+const {Sequelize, DataTypes, BelongsTo, HasMany} = require('sequelize')
 
 const sequelize = new Sequelize({
     dialect: "sqlite",
     storage: './database.sqlite'
 })
 
-module.exports = sequelize
+const DepartamentoModel = sequelize.define('Departamento', {
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    nome: {
+        type: DataTypes.STRING,
+        allowNull: false
+    }
+})
+
+const CursoModel = sequelize.define('Curso', {
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    nome: {
+        type: DataTypes.STRING,
+        allowNull: false
+    }
+
+})
+
+const ProfessorModel = sequelize.define('Professor', {
+    ra: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    nome: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+})
+
+const AlunoModel = sequelize.define('Aluno', {
+    ra: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    nome: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+})
+
+ProfessorModel.belongsTo(DepartamentoModel)
+CursoModel.hasMany(AlunoModel)
+CursoModel.belongsTo(DepartamentoModel) 
+CursoModel.hasMany(ProfessorModel)
+
+
+module.exports = {
+    sequelize: sequelize,
+    DepartamentoModel: DepartamentoModel,
+    ProfessorModel: ProfessorModel,
+    AlunoModel: AlunoModel,
+    CursoModel: CursoModel
+}
--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
-const sequelize = require('../model/bd')
+const {sequelize} = require('../model/bd')
 
 /* GET home page. */
-router.get('/', async function(req, res, next) {
-  await sequelize.sync()
-  res.json({msg: "Hello World!"});
+router.get('/install', async function(req, res, next) {
+  await sequelize.sync({force: true})
+  res.json({msg: "Instalado com sucesso!"});
 });
 
 module.exports = router;
